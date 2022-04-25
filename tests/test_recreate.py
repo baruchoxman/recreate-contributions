1828b039d75d22213f8df8cf246959636775ece7
Baruch Oxman <baruch.oxman@gmail.com>
2022-04-25 16:23:51 +0300

    Adding some tests with travis configuration

---
+++ tests/test_recreate.py
@@ -0,0 +1,18 @@
+import recreate
+import datetime
+import pytest
+
+
+@pytest.mark.parametrize("shell,expected", [
+    ("bash",
+     "GIT_AUTHOR_DATE=2020-01-01T12:00:00 GIT_COMMITTER_DATE=2020-01-01T12:00:00 "
+     "git commit --allow-empty -m \"recreating contributions\" > /dev/null\n"),
+    ("powershell",
+     "$Env:GIT_AUTHOR_DATE=\"2020-01-01T12:00:00\"\n"
+     "$Env:GIT_COMMITTER_DATE=\"2020-01-01T12:00:00\"\n"
+     "git commit --allow-empty -m \"recreating contributions\" | Out-Null\n"),
+])
+def test_commit_bahs(shell, expected):
+    test_date = datetime.date(2020,1,1)
+    res = recreate.commit(test_date, shell)
+    assert res == expected
