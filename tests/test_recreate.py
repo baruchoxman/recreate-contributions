606b5737a316ad0921563dd76899116f0fa151fc
Baruch Oxman <baruch.oxman@gmail.com>
2022-04-25 16:29:34 +0300

    Fix function name

--- tests/test_recreate.py
+++ tests/test_recreate.py
@@ -12,7 +12,7 @@ import pytest
      "$Env:GIT_COMMITTER_DATE=\"2020-01-01T12:00:00\"\n"
      "git commit --allow-empty -m \"recreating contributions\" | Out-Null\n"),
 ])
-def test_commit_bahs(shell, expected):
+def test_commit(shell, expected):
     test_date = datetime.date(2020,1,1)
     res = recreate.commit(test_date, shell)
     assert res == expected
