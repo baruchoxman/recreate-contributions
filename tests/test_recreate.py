038e0300444dd4eafc793a50cc3f32b9f20a005c
Baruch Oxman <baruch.oxman@gmail.com>
2022-04-25 16:34:39 +0300

    Reformatting with black

--- tests/test_recreate.py
+++ tests/test_recreate.py
@@ -3,16 +3,23 @@ import datetime
 import pytest
 
 
-@pytest.mark.parametrize("shell,expected", [
-    ("bash",
-     "GIT_AUTHOR_DATE=2020-01-01T12:00:00 GIT_COMMITTER_DATE=2020-01-01T12:00:00 "
-     "git commit --allow-empty -m \"recreating contributions\" > /dev/null\n"),
-    ("powershell",
-     "$Env:GIT_AUTHOR_DATE=\"2020-01-01T12:00:00\"\n"
-     "$Env:GIT_COMMITTER_DATE=\"2020-01-01T12:00:00\"\n"
-     "git commit --allow-empty -m \"recreating contributions\" | Out-Null\n"),
-])
+@pytest.mark.parametrize(
+    "shell,expected",
+    [
+        (
+            "bash",
+            "GIT_AUTHOR_DATE=2020-01-01T12:00:00 GIT_COMMITTER_DATE=2020-01-01T12:00:00 "
+            'git commit --allow-empty -m "recreating contributions" > /dev/null\n',
+        ),
+        (
+            "powershell",
+            '$Env:GIT_AUTHOR_DATE="2020-01-01T12:00:00"\n'
+            '$Env:GIT_COMMITTER_DATE="2020-01-01T12:00:00"\n'
+            'git commit --allow-empty -m "recreating contributions" | Out-Null\n',
+        ),
+    ],
+)
 def test_commit(shell, expected):
-    test_date = datetime.date(2020,1,1)
+    test_date = datetime.date(2020, 1, 1)
     res = recreate.commit(test_date, shell)
     assert res == expected
